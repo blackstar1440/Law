1b713d8903284b8f69ee25d5fe6c76c47317c968
DacerSensei <73899245+DacerSensei@users.noreply.github.com>
2020-12-16 10:06:51 +0800

    Initial commit

---
+++ Source/Law/Law.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Law.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Law, "Law" );
---
+++ Source/Law/LawGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LawGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LAW_API ALawGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
